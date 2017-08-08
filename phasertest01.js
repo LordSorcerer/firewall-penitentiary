cb0cc649169ab144781777a33640035cbfe0eb4e
LordSorcerer <ms1@thousandstorms.com>
2017-08-08 11:33:48 -0400

    Most network errors fixed.  Player shoots sideways. Crash on shield kill.

--- phasertest01.js
+++ phasertest01.js
@@ -12,37 +12,43 @@ app.get('/', function(req, res) {
 });
 
 var playerID = 0,
-    /* freePlayerID = [],
-     assignedPlayerID = [],*/
+    freeIDList = [],
     playerList = [],
     numPlayers = 0,
     gameActive = 0;
 
+var maxPlayers = 1;
+
+setupServer();
+
 io.on('connection', function(socket) {
     //Checks to see if the game is full.  If not, give the player an ID and increment the total number of players.
     socket.on('requestPlayerID', function() {
-        playerID = playerList.length;
-        if (playerID >= 0 && playerList.length < 4) {
+        if (freeIDList.length > 0) {
+            playerID = freeIDList[0];
+            freeIDList.splice(0, 1);
             socket.emit('playerID', playerID);
             console.log("Player #" + playerID + " has joined on socket " + socket.id + ".");
-            playerList.push({ "socket": socket.id, "playerID": playerID });
-            console.log("Player socket list: " + playerList);
+            playerList.push({ 'socketID': socket.id, 'playerID': playerID });
+             console.log("FreeIDList(" + freeIDList.length + " slot(s) available): " + freeIDList);
             //The Server chats with either the joining player's socket or ALL players' sockets
-            socket.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "Welcome to Firewall Penitentiary.  Your body has been immobilized until the start of the game.  Wait patiently, Player#" + playerID + "."});
-            if (playerList.length >= 4) {
-                io.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "The final player has joined.  Five seconds until game time..." });
+            if (freeIDList.length <= 0) {
+                io.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "The final player has joined.  Ten seconds until game time." });
                 setTimeout(function() {
                     io.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "The game has begun!" });
                     gameActive = 1;
-                }, 5000);
+                }, 12000);
+                io.emit('music', 1);
             } else {
-                socket.broadcast.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "A new player has joined the game.  Welcome, Player#" + playerID });
+                socket.broadcast.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "A new player has joined the game.  Welcome, Player#" + playerID + "." });
+                socket.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "Welcome to Firewall Penitentiary.  Your body has been immobilized until the start of the game.  Wait patiently, Player#" + playerID + "." });
             }
 
         } else {
             socket.emit('playerID', -1);
             console.log("Socket: " + socket.id + ", game full.  Enabling observation mode.");
             socket.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "The game is full.  Switching to observation mode." });
+            socket.emit('music', 1);
         };
     });
 
@@ -59,19 +65,42 @@ io.on('connection', function(socket) {
 
     socket.on('disconnect', function() {
         //get the disconnected socket ID and remove it from the player list
-        console.log("Socket: " + socket);
-        /*Object.keys()*/
-        var index = playerList.indexOf(socket.id);
+        var index = matchPlayerSocketID(socket.id);
         console.log("Index: " + index);
         //must also get the player's ID so we can free it for use
         if (index != -1) {
+            var tempPlayerID = playerList[index].playerID;
             playerList.splice(index, 1);
+            freeIDList.push(tempPlayerID);
+            console.log(freeIDList);
         };
         console.info("Socket " + socket.id + " has disconnected.");
-        io.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: + "Player#" + index + " has disconnected." });
+        io.emit('chat', { sender: "Server", fontColor: "#FF00FF", message: "Player#" + tempPlayerID + " has disconnected." });
     });
 });
 
 server.listen(port, function() {
     console.log("Firewall Penitentiary Server v1.0  Listening on Port#: " + port);
+    console.log("FreeIDList(" + freeIDList.length + " slot(s) available): " + freeIDList);
 });
+
+
+
+function matchPlayerSocketID(currentSocketID) {
+    var ret = -1;
+    for (i = 0; i < playerList.length; i++) {
+        console.log("ID: " + playerList[i].socketID + " id: " + currentSocketID);
+        if (playerList[i].socketID == currentSocketID) {
+            ret = i;
+        };
+    };
+    return ret;
+};
+
+//Create a number of playerIDs equal to maxPlayers and put them in the freeIDList
+function setupServer() {
+    for (i = 0; i < maxPlayers; i++) {
+        freeIDList.push(i);
+    }
+
+}
